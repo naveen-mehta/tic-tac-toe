8775f25a9c98c88195984a7d897f11c31b4d2d88
Naveen <navi.mehta@gmail.com>
2020-09-30 22:48:31 +1000

    added logic to update playerClicks. added checkWinner function and win combinations array

---
+++ Resources/planning-sheet.md
@@ -0,0 +1,37 @@
+variables - 
+1. active player
+2. 
+
+- active player variable: set to player 1 as default 
+
+** handleClick function **
+
+- if player 1 selected {
+    - show X
+    - update player 1 choice / box clicked
+    - checkWinner function (); 
+    - switch active player to player 2 
+  } else {
+    - show O
+    - update player 2 choice / box clicked
+    - checkWinner function ();
+    - switch active player to player 1
+  }
+  
+** checkWinner function **
+
+winning combinations  
+- 123
+- 456
+- 789
+- 147
+- 258
+- 369
+- 159
+- 357
+
+
+
+
+
+
--- app.js
+++ app.js
@@ -1,13 +1,33 @@
-var activePlayer = "Player 1"
-var grid = document.querySelectorAll('.box')
-var player1selection = [];
+var activePlayer = "Player 1";
+var grid = document.querySelectorAll('.box');
+var player1clicks = [];
+var player2clicks = [];
+var allClicks = []; //tracks clicks for both players 
+var matchTracker = [];
+var winCombinations = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7]
+];
 
 function handleClick(event) {
+    var boxNum = event.target.dataset.id;
     if (activePlayer == "Player 1") {
         event.target.textContent = "X";
+        allClicks.push(boxNum); 
+        player1clicks.push(boxNum);
+        checkWinner(player1clicks);
         switchActivePlayer("Player 2");
     } else {
         event.target.textContent = "O";
+        allClicks.push(boxNum);
+        player2clicks.push(boxNum);
+        checkWinner(player2clicks);
         switchActivePlayer("Player 1");
     }
 }
@@ -19,5 +39,21 @@ function switchActivePlayer(playerName) {
 //event listener to handle user clicks
 for (var i = 0; i < 9; i++) {
     grid[i].addEventListener('click', handleClick);
+}
+
+function checkWinner(playerClicks) {
+    for (var i = 0; i < winCombinations.length; i++) {
+        for (var j = 0; j < 3; j++) {
+            if (playerClicks.includes(winCombinations[i][j])) {
+                matchTracker.push(winCombinations[i][j]);
+            }    
+        } 
+        if (matchTracker.length === 3) {
+            return matchTracker;
+        } else {
+            matchTracker = [];
+        }
+    }
+    return matchTracker;
 }
 
